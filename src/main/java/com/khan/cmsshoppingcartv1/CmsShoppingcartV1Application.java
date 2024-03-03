8857b36b70646490ded5aa8bbf4bbd9f8107bba0
Imran Khan <imranahmedkhan@hotmail.co.uk>
2024-03-03 15:53:12 +0000

    Project Started - basic template

---
+++ src/main/java/com/khan/cmsshoppingcartv1/CmsShoppingcartV1Application.java
@@ -0,0 +1,13 @@
+package com.khan.cmsshoppingcartv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsShoppingcartV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsShoppingcartV1Application.class, args);
+	}
+
+}
