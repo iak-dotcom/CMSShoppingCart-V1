415e793ee08814f5ee4ee3c411c6295710e90a33
Imran Khan <imranahmedkhan@hotmail.co.uk>
2024-03-14 23:56:31 +0000

    admin structure created, changes made in nav.html for seperating user and admin

---
+++ src/main/java/com/khan/cmsshoppingcartv1/controllers/AdminPagesController.java
@@ -0,0 +1,14 @@
+package com.khan.cmsshoppingcartv1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/pages")
+public class AdminPagesController {
+    @GetMapping
+    public String index(){
+        return "admin/pages/index";
+    }
+}
