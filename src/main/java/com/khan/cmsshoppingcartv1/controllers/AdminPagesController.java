be0e64375c797c44a4ed35298a4aea51d1bb1187
Imran Khan <imranahmedkhan@hotmail.co.uk>
2024-03-15 02:35:40 +0000

    Admin Page edit has been configured

--- src/main/java/com/khan/cmsshoppingcartv1/controllers/AdminPagesController.java
+++ src/main/java/com/khan/cmsshoppingcartv1/controllers/AdminPagesController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -66,5 +67,45 @@ public String add(@Valid Page page, BindingResult bindingResult, RedirectAttribu
 
     return "redirect:/admin/pages/add"; // Redirect regardless of whether the page was saved or not
 }
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable int id, Model model) {
 
+        Page page = pageRepo.getOne(id);
+
+        model.addAttribute("page", page);
+
+        return "admin/pages/edit";
+        
+    }
+
+    @PostMapping("/edit")
+    public String edit(@Valid Page page, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        Page pageCurrent = pageRepo.getOne(page.getId());
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("pageTitle", pageCurrent.getTitle());
+            return "admin/pages/edit";
+        }
+
+        redirectAttributes.addFlashAttribute("message", "Page edited");
+        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+        String slug = page.getSlug() == "" ? page.getTitle().toLowerCase().replace(" ", "-") : page.getSlug().toLowerCase().replace(" ", "-");
+
+        Page slugExists = pageRepo.findBySlugAndIdNot(slug, page.getId());
+
+        if ( slugExists != null ) {
+            redirectAttributes.addFlashAttribute("message", "Slug exists, choose another");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+            redirectAttributes.addFlashAttribute("page", page);
+
+        } else {
+            page.setSlug(slug);
+
+            pageRepo.save(page);
+        }
+
+        return "redirect:/admin/pages/edit/" + page.getId();
+    }
 }
