f729ad6bd1418d7b39bd4ff17d791b5d0771046e
Imran Khan <imranahmedkhan@hotmail.co.uk>
2024-03-15 00:27:08 +0000

    setup the JPARepo for Admin Pages and autowired to AdminPageController

--- src/main/java/com/khan/cmsshoppingcartv1/controllers/AdminPagesController.java
+++ src/main/java/com/khan/cmsshoppingcartv1/controllers/AdminPagesController.java
@@ -1,14 +1,27 @@
 package com.khan.cmsshoppingcartv1.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.khan.cmsshoppingcartv1.models.PageRepository;
+import com.khan.cmsshoppingcartv1.models.data.Page;
 
 @Controller
 @RequestMapping("/admin/pages")
 public class AdminPagesController {
+
+@Autowired
+private PageRepository pageRepo;
+
     @GetMapping
-    public String index(){
+    public String index(Model model){
+        List<Page> pages= pageRepo.findAll();
+        model.addAttribute("pages", pages);
         return "admin/pages/index";
     }
 }
