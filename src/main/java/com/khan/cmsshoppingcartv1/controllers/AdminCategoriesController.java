919c2f85c6309aa37d666ac9d0a74ce61f462e56
Imran Khan <imranahmedkhan@hotmail.co.uk>
2024-03-16 14:15:27 +0000

    Category Edit and post configured

--- src/main/java/com/khan/cmsshoppingcartv1/controllers/AdminCategoriesController.java
+++ src/main/java/com/khan/cmsshoppingcartv1/controllers/AdminCategoriesController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -34,11 +35,13 @@ public class AdminCategoriesController {
     }
 
     @GetMapping("/add")
-    public String add(Category category){
+    public String add(Category category) {
         return "admin/categories/add";
     }
-@PostMapping("/add")
-    public String add(@Valid Category category, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+    @PostMapping("/add")
+    public String add(@Valid Category category, BindingResult bindingResult, RedirectAttributes redirectAttributes,
+            Model model) {
 
         if (bindingResult.hasErrors()) {
             return "admin/categories/add";
@@ -51,7 +54,7 @@ public class AdminCategoriesController {
 
         Category categoryExists = categoryRepo.findByName(category.getName());
 
-        if ( categoryExists != null ) {
+        if (categoryExists != null) {
             redirectAttributes.addFlashAttribute("message", "Category exists, choose another");
             redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
             redirectAttributes.addFlashAttribute("categoryInfo", category);
@@ -65,4 +68,45 @@ public class AdminCategoriesController {
 
         return "redirect:/admin/categories/add";
     }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable int id, Model model) {
+
+        Category category = categoryRepo.getOne(id);
+
+        model.addAttribute("category", category);
+
+        return "admin/categories/edit";
+
+    }
+    @PostMapping("/edit")
+    public String edit(@Valid Category category, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        Category categoryCurrent = categoryRepo.getOne(category.getId());
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("categoryName", categoryCurrent.getName());
+            return "admin/categories/edit";
+        }
+
+        redirectAttributes.addFlashAttribute("message", "Category edited");
+        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+        String slug = category.getName().toLowerCase().replace(" ", "-");
+
+        Category categoryExists = categoryRepo.findByName(category.getName());
+
+        if ( categoryExists != null ) {
+            redirectAttributes.addFlashAttribute("message", "Category exists, choose another");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+
+        } else {
+            category.setSlug(slug);
+
+            categoryRepo.save(category);
+        }
+
+        return "redirect:/admin/categories/edit/" + category.getId();
+    }
+
 }
