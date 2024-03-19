eecc51b49d3ead044b932ac5f32005914a7403e5
Imran Khan <imranahmedkhan@hotmail.co.uk>
2024-03-19 05:03:17 +0000

    Home page added

--- src/main/java/com/khan/cmsshoppingcartv1/WebConfig.java
+++ src/main/java/com/khan/cmsshoppingcartv1/WebConfig.java
@@ -2,7 +2,7 @@ package com.khan.cmsshoppingcartv1;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+// import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     // If you want to specify/override the path
     // @Override
     // public void addViewControllers(ViewControllerRegistry registry) {
-    //     registry.addViewController("/").setViewName("home");
+    //     registry.addViewController("/").setViewName("page");
     // }
 
     @Override
---
+++ src/main/java/com/khan/cmsshoppingcartv1/controllers/PagesController.java
@@ -0,0 +1,49 @@
+package com.khan.cmsshoppingcartv1.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.khan.cmsshoppingcartv1.models.PageRepository;
+import com.khan.cmsshoppingcartv1.models.data.Page;
+
+
+@Controller
+ @RequestMapping("/")
+public class PagesController {
+
+    @Autowired
+    private PageRepository pageRepo;
+
+    @GetMapping
+    public String home(Model model) {
+        
+        Page page = pageRepo.findBySlug("home");
+        model.addAttribute("page", page);
+        
+        return "page";
+    }
+
+    // @GetMapping("/login")
+    // public String login() {
+    //     return "login";
+    // }
+
+    // @GetMapping("/{slug}")
+    // public String page(@PathVariable String slug, Model model) {
+        
+    //     Page page = pageRepo.findBySlug(slug);
+
+    //     if (page == null) {
+    //         return "redirect:/";
+    //     }
+        
+    //     model.addAttribute("page", page);
+        
+    //     return "page";
+    // }
+    
+}
