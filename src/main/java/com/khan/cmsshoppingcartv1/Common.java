6db1a6b964603c720550d74bf2d08ccfe2ff2b65
Imran Khan <imranahmedkhan@hotmail.co.uk>
2024-03-20 02:07:17 +0000

    Full Cart Functionality added

--- src/main/java/com/khan/cmsshoppingcartv1/Common.java
+++ src/main/java/com/khan/cmsshoppingcartv1/Common.java
@@ -10,13 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
-
+import javax.servlet.http.HttpSession;
+import com.khan.cmsshoppingcartv1.models.Cart;
 import com.khan.cmsshoppingcartv1.models.CategoryRepository;
 import com.khan.cmsshoppingcartv1.models.PageRepository;
 import com.khan.cmsshoppingcartv1.models.data.Category;
 import com.khan.cmsshoppingcartv1.models.data.Page;
 
 @ControllerAdvice
+@SuppressWarnings("unchecked")
 public class Common {
     @Autowired
     private PageRepository pageRepo;
@@ -24,15 +26,29 @@ public class Common {
     private CategoryRepository categoryRepo;
 
     @ModelAttribute
-    public void sharedData(Model model){
-     
-
-        
+    public void sharedData(Model model,HttpSession session){
+         
 
         List<Category> categories = categoryRepo.findAllByOrderBySortingAsc();
         List<Page> pages = pageRepo.findAllByOrderBySortingAsc();
-        
+        boolean cartActive = false;
+        if(session.getAttribute("cart")!=null){
+            HashMap<Integer, Cart> cart = (HashMap<Integer,Cart>) session.getAttribute("cart");
+            int size =0;
+            double total =0;
+
+            for (Cart value: cart.values()){
+                size+=value.getQuantity();
+                total+=value.getQuantity() * Double.parseDouble(value.getPrice());
+            }
+            model.addAttribute("csize", size);
+            model.addAttribute("ctotal", total);
+
+            cartActive =true;
+        }
+
         model.addAttribute("ccategories", categories);
         model.addAttribute("cpages",pages);
+        model.addAttribute("cartActive", cartActive);
     }
 }
